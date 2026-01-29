50e78ff339e0a04e9330b98b47fcf097ccdbd08e
“JuanPa0000” <jfuentes20943@iesjoanramis.org>
2026-01-29 14:04:17 +0100

    improve design

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Header from "../components/Header"
 import { useNavigate } from "react-router";
 
 export default function Profile() {
+
+    const [userInfoList, setUserInfoList] = useState([]);
 
     const navigate = useNavigate();
 
@@ -23,6 +25,7 @@ export default function Profile() {
                 }
             })
             const data = await res.json();
+            setUserInfoList(data);
             console.log(data);
         }
         getUserInfo();
@@ -37,8 +40,20 @@ export default function Profile() {
                     <img src="../../public/profile-avatar.webp" className="w-full shadow-xl rounded-[100%] border-4 border-[var(--fondo)] w-[12rem] transform translate-y-[7rem]"/>
                 </div>
 
-                <div>
-
+                <div className="pl-[13rem] flex text-[var(--letra)] justify-between h-full">
+                    <div>
+                        <h1 className="font-bold text-[2rem] ">{userInfoList.username}</h1>
+                        <p className="text-gray-400">{userInfoList.email}</p>
+                        <div className="flex gap-10 text-lg pt-2 pb-2">
+                            <p><i className="fa-solid fa-graduation-cap"></i>Aula: <span className="text-black font-bold">{userInfoList.aula}</span></p>
+                            <p><i className="fa-solid fa-star"></i>Tutor: <span className="text-black font-bold">{userInfoList.tutor}</span></p>
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <button>
+                            <i className="fa-solid fa-pencil"></i>
+                        </button>
+                    </div>
                 </div>
             </section>
         </div>
